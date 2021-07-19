3508ab5d4dc5cdf11523ef8574e9bdb9b2c3ce7f
mpadil18 <mpadil18@ucsc.edu>
2021-07-19 14:35:52 -0700

    Updated CSS and placed component in App for testing

---
+++ frontend/src/components/Menu/Menu.js
@@ -0,0 +1,98 @@
+import React, { useState } from 'react';
+import './Menu.css'
+import { TextField } from '@material-ui/core';
+import Multiselect from 'multiselect-react-dropdown';
+
+import pencil from '../../assets/pencil.svg'
+
+var selectedCategories = []
+
+var userValues = [];
+var userTime = "";
+var userMeal = "";
+var userCategories = [];
+
+//Pass elements into popup with props.children
+export default function Menu() {
+    const data = [
+        {Category: 'Vegetarian', id:1},
+        {Category: 'Vegan', id:2},
+        {Category: 'Gluten Free', id:3},
+        {Category: 'Dairy Free', id:4},
+        {Category: 'Breakfast', id:5},
+        {Category: 'Main Course', id:6},
+        {Category: 'Side Dish', id:7},
+        {Category: 'Salad', id:8},
+        {Category: 'Appetizer', id:9},
+        {Category: 'Soup', id:10},
+        {Category: 'Finger Food', id:11},
+        {Category: 'Drink', id:12}
+    ]
+
+    const [options] = useState(data)
+    const [submitIsVisible, setSubmitIsVisible] = useState(false)
+
+    const handleOpenButton = () => {
+        setSubmitIsVisible(true)
+    }
+    
+    const handleCloseButton = () => {
+        setSubmitIsVisible(false)
+    }
+
+    const handleButton = () => {
+        console.log(document.getElementById('time').value)
+        console.log((document.getElementById('meal-name').value))
+        console.log(selectedCategories)
+        if ((document.getElementById('time').value !== "") && (document.getElementById('meal-name').value !== "") && (selectedCategories.length !== 0)) {
+            console.log("ALL FIELDS COMPLETE")
+            handleOpenButton();
+        }
+        else {
+            handleCloseButton();
+        }
+    }
+
+    const onSelect = (data) => {
+        selectedCategories = data;
+        console.log(selectedCategories)
+    }
+
+    const returnValues = () => {
+        userTime = document.getElementById('time').value;
+        userMeal =document.getElementById('meal-name').value;
+        userCategories = selectedCategories;
+        userValues = [userTime, userMeal, userCategories]
+        console.log("HERE ARE THE VALUES", userValues)
+        //return userValues;
+    }
+
+    return (
+        <div className="menu">
+            
+            <div className="menuLine">
+                <p>Time to eat: </p>
+                <div className="userInput"><TextField type="time" id="time" onChange={(handleButton)}></TextField></div>
+            </div>
+            <div className="menuLine">
+                <p>Meal Name: </p>
+                <div className="fake-input menu-input">
+                    <input type="text" id="meal-name" required placeholder="Ex: Breakfast, lunch..." onChange={(handleButton)} onClick={(handleButton)}></input>
+                    <img src={pencil} width="25" alt="Edit your plan name"></img>
+                </div>
+            </div>
+            <div className="menuLine">
+                <p>Categories: </p>
+                <div onClick={(handleButton)}><div className="setWidth"><Multiselect options={data} displayValue={"Category"} onSelect={onSelect} id="category"></Multiselect></div></div>
+            </div>
+            
+            {submitIsVisible ?
+                <>
+                <div className="submit-but"><button onClick={(returnValues)}>Close</button></div>
+                </> : 
+                ""
+            }
+
+        </div>
+    )
+}
--- frontend/src/components/Wheel/Wheel.js
+++ frontend/src/components/Wheel/Wheel.js
@@ -1,9 +1,100 @@
+import { useState } from 'react';
 import './Wheel.css'
+import { Wheel } from 'react-custom-roulette'
+import Popup from '../Popup/Popup'
+import Menu from '../Menu/Menu'
 
-export default function Wheel() {
+import pencil from '../../assets/pencil.svg'
+
+export default function Generator() {
+  const [wheelIsVisible, setWheelIsVisible] = useState(true)
+  const [mustSpin, setMustSpin] = useState(false);
+  const [prizeNumber, setPrizeNumber] = useState(0);
+  //for recipe popup
+  const [buttonPopup, setButtonPopup] = useState(false);
+
+  const data = [
+    { option: 'a', style: { backgroundColor: 'green', textColor: 'black' }, category: 1},
+    { option: 'b', category: 0},
+    { option: 'c', category: 1},
+  ]
+
+  //Use a query to get all the recipes that fit the category selected.
+  //SELECT id or name FROM all_recipes WHERE category = [SELECTED CATEGORY FROM WHEEL]
+  //load options into an array like so: 
+  for (let i = 0; i < 30; i++) {
+    data.push({ option: 'a', category: 0})
+  }
+
+  console.log(data)
+
+  const handleSpinClick = () => {
+    const newPrizeNumber = Math.floor(Math.random() * data.length)
+    console.log(newPrizeNumber)
+    setPrizeNumber(newPrizeNumber)
+    setMustSpin(true)
+    console.log(data[newPrizeNumber].option)
+    console.log(data[prizeNumber].category)
+  }
+  
+  const handleOpenForm = () => {
+    setWheelIsVisible(false)
+  }
+
+  const handleCloseForm = () => {
+    setWheelIsVisible(true)
+  }
+
   return (
-    <div className="Wheel">
+    <div className="Generator">
+      <div className="header">Spin the wheel to plan your meal</div>
+      <div className="fullPage">
+        <div className="menuView">
+          <div className="fake-input">
+              <input type="text" required placeholder="Name your meal plan!"></input>
+              <img src={pencil} width="25" alt="Edit your plan name"></img>
+          </div>
+          <div className="subHeader">Meals</div>
+          <div className="box"><div className="penIc"><div onClick={(handleOpenForm)}><img src="https://i.imgur.com/SR5qJxc.png" className="add" alt="add meal"></img></div></div></div>
+        </div>
+        <div className="wheelView">
+        {wheelIsVisible ?
+        <>
+        {/* https://github.com/effectussoftware/react-custom-roulette */}
+          <Wheel
+            mustStartSpinning={mustSpin}
+            prizeNumber={prizeNumber}
+            data={data}
 
+            onStopSpinning={() => {
+              setMustSpin(false)
+              setButtonPopup(true)
+            }}
+          />
+          <button onClick={handleSpinClick}>SPIN</button>
+        </> : 
+        <div className="menuParent">
+          <div className="menuHeader">
+          <div className="close"><div onClick={(handleCloseForm)}><img src="https://svgshare.com/i/ZL_.svg"></img></div></div>
+            <div className="menuItem">Edit a meal</div>
+          </div>
+          <Menu></Menu>
+        </div>
+        }
+      </div>
+      </div>
+
+      {/* https://www.npmjs.com/package/react-wheel-of-prizes?activeTab=readme */}
+
+      <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
+          <h3>My popup</h3>
+          
+          <p>This is my button triggered popup</p>
+
+          {(data[prizeNumber].option)}
+          {(data[prizeNumber].category)}
+
+      </Popup>
     </div>
   )
 }
