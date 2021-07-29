4410002f02faf6e786f459b9e2fc818012f67ba5
Ann <annie.wang50@yahoo.com>
2021-07-29 18:45:49 -0400

    about us page skeleton

--- frontend/src/components/About/About.js
+++ frontend/src/components/About/About.js
@@ -2,9 +2,44 @@
 import "./About.css"
 
 export default function About() {
+  const aboutus = [
+    {
+      name: "Maritza Padilla",
+      bio: ""
+    },
+    {
+      name: "Andrew Lee",
+      bio: ""
+    },
+    {
+      name: "Ann Wang",
+      bio: ""
+    }
+  ];
+
   return (
     <div className="About">
-      <div>About us page</div>
+
+      <section className="mission">
+        <div>Our Mission</div>
+        <p>
+          To provide a place where working people can quickly and easily access inexpensive 
+          recipes that align with their needs, as well as plan out meals using a random recipe 
+          generator specific to their inputted criteria.
+        </p>
+      </section>
+
+      <section className="aboutus">
+        <div>About Us</div>
+        <div className="people">
+          {aboutus.map((item, i) => (
+            <div key={i} className="person">
+              <div>name</div>
+              <p>content</p>
+            </div>
+          ))}
+        </div>
+      </section>
     </div>
   )
 }
