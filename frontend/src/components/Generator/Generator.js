14254e30121ced403866193af734bdea694e2ab4
mpadil18 <mpadil18@ucsc.edu>
2021-08-03 11:58:04 -0700

    Changed flex on wheel, added navigate + verification, need error handle for title

--- frontend/src/components/Generator/Generator.js
+++ frontend/src/components/Generator/Generator.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
-import './Wheel.css'
+import { useNavigate, Link } from "react-router-dom";
+import './Generator.css'
 import { Wheel } from 'react-custom-roulette'
 import Popup from '../Popup/Popup'
 import Menu from '../Menu/Menu'
@@ -32,6 +33,7 @@ export default function Generator({user}) {
   const [mealPlanPopup, setMealPlanPopup] = useState(false);
   const [spinIsVisible, setSpinIsVisible] = useState(false);
   const [boxVals, setBoxVals] = useState(-1)
+  const navigate = useNavigate();
   const [loadWheel, setLoadWheel] = useState([
     { option: "meal to"},
     { option: "begin"},
@@ -148,131 +150,119 @@ export default function Generator({user}) {
     const { data, err } = await apiClient.saveMealPlan(obj);
     if (data) {
       console.log("Successful load!")
+      navigate("/saved");
     }
     console.log("Recipe list: ", recipes)
   }
 
-  const mealIn = (i) => {
-    return (
-      <div>
-        <p>{titles[i]}</p>
-        <p>{images[i]}</p>
-        <p>{preps[i]}</p>
-        <p>{ratings[i]}</p>
-      </div>
-    )
-  }
-
-  const mapMealPlan = () => {
-    for (let i = 0; i < titles.length; i++) {
-      var x = mealIn(i)
-      console.log("PRP",x.props.children)
-      console.log(mealIn(i))
-      //return [titles, images, preps, ratings];
-    }
-  }
-
   return (
-    <div className="Generator">
-      <div className="header">Spin the wheel to plan your meal</div>
-      <div className="fullPage">
-        <div className="menuView">
-          <div className="fake-input">
-              <input type="text" required placeholder="Name your meal plan!" id="mealPlanTitle" ></input>
-              <img src={pencil} width="25" alt="Edit your plan name"></img>
-          </div>
-          <div className="subHeader">Meals</div>
-          {/*<div className="box"><p>{times[dum]}</p><p>{meals[dum]}</p><div className="penIc"><div onClick={(handleOpenForm)}><img src="https://i.imgur.com/SR5qJxc.png" className="add" alt="add meal"></img></div></div></div>*/}
-          <div className="leftMenuArea"><RenderBox handleOpenForm = {handleOpenForm} dataWheelToBox={dum} renderMealData={[times, meals]}/></div>
-          {dum >= 1 ?
-                <>
-                  <div onClick={()=>setMealPlanPopup(true)}><button className="saveMealPlan">Save Meal Plan</button></div>
-                </> : 
-                ""
-          }
+    <div className="Wheel">
+      {!user.email ? (
+        <div className="WheelMessage">
+          Login <Link to="/login">here</Link> to spin the wheel and plan your meal!
         </div>
-        <div className="wheelView">
-        {wheelIsVisible ?
-        <>
-        {/* https://github.com/effectussoftware/react-custom-roulette */}
-          <Wheel
-            mustStartSpinning={mustSpin}
-            prizeNumber={prizeNumber}
-            data={loadWheel}
-
-            onStopSpinning={() => {
-              setMustSpin(false)
-              setButtonPopup(true)
-            }}
-          />
-          {spinIsVisible ?
-                <>
-                <button onClick={handleSpinClick}>SPIN</button>
-                </> : 
-                ""
-          }
-        </> : 
-        <div className="menuParent">
-          <div className="menuHeader">
-          <div className="close"><div onClick={(handleCloseForm)}><img src="https://svgshare.com/i/ZL_.svg"></img></div></div>
-            <div className="menuItem">Edit a meal</div>
-          </div>
-          <Menu data={handleCloseForm} onClick2={setSpinIsVisible} passToParent={dummyLoadWheel}></Menu>
-        </div>
-        }
-      </div>
-      </div>
-
-      {/* https://www.npmjs.com/package/react-wheel-of-prizes?activeTab=readme */}
-
-      <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
-        {spinIsVisible ?
+      ) : (
+      <div className="Generator">
+        <div className="header">Spin the wheel to plan your meal</div>
+        <div className="fullPage">
+          <div className="menuView">
+            <div className="fake-input">
+                <input type="text" required placeholder="Name your meal plan!" id="mealPlanTitle" ></input>
+                <img src={pencil} width="25" alt="Edit your plan name"></img>
+            </div>
+            <div className="subHeader">Meals</div>
+            {/*<div className="box"><p>{times[dum]}</p><p>{meals[dum]}</p><div className="penIc"><div onClick={(handleOpenForm)}><img src="https://i.imgur.com/SR5qJxc.png" className="add" alt="add meal"></img></div></div></div>*/}
+            <div className="leftMenuArea"><RenderBox handleOpenForm = {handleOpenForm} dataWheelToBox={dum} renderMealData={[times, meals]}/></div>
+            {dum >= 1 ?
                   <>
-                    {console.log("CRASH AT", data)}
-                    {(data.length !== 0) && (data != null) && (typeof (data[prizeNumber]) !== 'undefined') ?
-                          <>
-                          {console.log("FINAL DATA", data)}
-                          {console.log("aaaaaAAAAAAAAAAAA", (displayRecipePopup((data[prizeNumber].option)).then(data=>console.log("AAAAAA",data))))}
-                          </> : 
-                          ""
-                    }
-                    {console.log("Is this it?", recipes[numMeal])}
-                    {typeof recipes[numMeal] !== 'undefined' ?
-                    <>
-                      {console.log("RECX", recipes[numMeal], recipes[numMeal].title, recipes[numMeal])}
-                      <RecipeCard
-                        user={user}
-                        recipeInfo={recipes[numMeal]}
-                        handleLinks={(false)}
-                      />
-                      <button className="keepBut" onClick={keepMeal}>Keep</button>
-                    </> :
-                    ""}
+                    <div onClick={()=>setMealPlanPopup(true)}><button className="saveMealPlan">Save Meal Plan</button></div>
                   </> : 
                   ""
-        }
-      </Popup>
-
-      <Popup trigger={mealPlanPopup} setTrigger={setMealPlanPopup}>
-        <h3 className="mealPlanHead">You did it! (almost)</h3>
-        {/* {mapMealPlan()} */}
-        <div className="mealPlan">
-        {titles.length > 0
-        ? titles.map((element, idx) => (
-          <div className="mod-box">
-            <RecipeCard
-              user={user}
-              recipeInfo={recipes[idx]}
-              handleLinks={(false)}
-            />
-            {/* {times[idx]} {meals[idx]} {titles[idx]} <img src={images[idx]}></img> {preps[idx]} {ratings[idx]} */}
+            }
           </div>
-        ))
-        : null}
-        </div>
-        <button className="keepBut" onClick={logMealPlan}>Keep Meal</button>
-      </Popup>
+          <div className="wheelView">
+          {wheelIsVisible ?
+          <>
+          {/* https://github.com/effectussoftware/react-custom-roulette */}
+            <Wheel
+              mustStartSpinning={mustSpin}
+              prizeNumber={prizeNumber}
+              data={loadWheel}
 
+              onStopSpinning={() => {
+                setMustSpin(false)
+                setButtonPopup(true)
+              }}
+            />
+            {spinIsVisible ?
+                  <>
+                  <button onClick={handleSpinClick}>SPIN</button>
+                  </> : 
+                  ""
+            }
+          </> : 
+          <div className="menuParent">
+            <div className="menuHeader">
+            <div className="close"><div onClick={(handleCloseForm)}><img src="https://svgshare.com/i/ZL_.svg"></img></div></div>
+              <div className="menuItem">Edit a meal</div>
+            </div>
+            <Menu data={handleCloseForm} onClick2={setSpinIsVisible} passToParent={dummyLoadWheel}></Menu>
+          </div>
+          }
+        </div>
+        </div>
+
+        {/* https://www.npmjs.com/package/react-wheel-of-prizes?activeTab=readme */}
+
+        <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
+          {spinIsVisible ?
+                    <>
+                      {console.log("CRASH AT", data)}
+                      {(data.length !== 0) && (data != null) && (typeof (data[prizeNumber]) !== 'undefined') ?
+                            <>
+                            {console.log("FINAL DATA", data)}
+                            {console.log("aaaaaAAAAAAAAAAAA", (displayRecipePopup((data[prizeNumber].option)).then(data=>console.log("AAAAAA",data))))}
+                            </> : 
+                            ""
+                      }
+                      {console.log("Is this it?", recipes[numMeal])}
+                      {typeof recipes[numMeal] !== 'undefined' ?
+                      <>
+                        {console.log("RECX", recipes[numMeal], recipes[numMeal].title, recipes[numMeal])}
+                        <RecipeCard
+                          user={user}
+                          recipeInfo={recipes[numMeal]}
+                          handleLinks={(false)}
+                        />
+                        <button className="keepBut" onClick={keepMeal}>Keep</button>
+                      </> :
+                      ""}
+                    </> : 
+                    ""
+          }
+        </Popup>
+
+        <Popup trigger={mealPlanPopup} setTrigger={setMealPlanPopup}>
+          <h3 className="mealPlanHead">You did it! (almost)</h3>
+          {/* {mapMealPlan()} */}
+          <div className="mealPlan">
+          {titles.length > 0
+          ? titles.map((element, idx) => (
+            <div className="mod-box">
+              <RecipeCard
+                user={user}
+                recipeInfo={recipes[idx]}
+                handleLinks={(false)}
+              />
+              {/* {times[idx]} {meals[idx]} {titles[idx]} <img src={images[idx]}></img> {preps[idx]} {ratings[idx]} */}
+            </div>
+          ))
+          : null}
+          </div>
+          <button className="keepBut" onClick={logMealPlan}>Keep Meal</button>
+        </Popup>
+      </div>
+      )}
     </div>
   )
 }
