e50c68840325425f86a9740d0e7f1a27c40975dd
user name test <alee322@mail.ccsf.edu>
2021-08-05 15:29:20 -0700

    styles added confirmdialog.js

--- frontend/src/components/Comment/ConfirmDialog.js
+++ frontend/src/components/Comment/ConfirmDialog.js
@@ -1,4 +1,9 @@
-import { makeStyles } from "@material-ui/core/styles";
+import React from "react";
+import {
+    createTheme,
+    makeStyles,
+    ThemeProvider,
+} from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import Avatar from "@material-ui/core/Avatar";
 import List from "@material-ui/core/List";
@@ -7,10 +12,29 @@ import ListItemAvatar from "@material-ui/core/ListItemAvatar";
 import ListItemText from "@material-ui/core/ListItemText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
 import PersonIcon from "@material-ui/icons/Person";
 import AddIcon from "@material-ui/icons/Add";
 import Typography from "@material-ui/core/Typography";
-import { blue } from "@material-ui/core/colors";
+import { blue, purple, green } from "@material-ui/core/colors";
+import Slide from "@material-ui/core/Slide";
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+});
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: blue[900],
+        },
+        secondary: {
+            main: "#ed4b82",
+        },
+    },
+});
 
 const emails = ["username@gmail.com", "user02@gmail.com"];
 const useStyles = makeStyles({
@@ -22,7 +46,8 @@ const useStyles = makeStyles({
 
 export default function ConfirmDialog(props) {
     const classes = useStyles();
-    const { onClose, selectedValue, open, handleDelete, comment, setAnchorEl } = props;
+    const { onClose, selectedValue, open, handleDelete, comment, setAnchorEl } =
+        props;
 
     const handleClose = () => {
         onClose(selectedValue);
@@ -40,19 +65,36 @@ export default function ConfirmDialog(props) {
     const handleClickNo = () => {
         handleClose();
         setAnchorEl(null);
-    }
+    };
 
     return (
         <Dialog
             onClose={handleClose}
             aria-labelledby="simple-dialog-title"
             open={open}
+            // TransitionComponent={Transition}
         >
             <DialogTitle id="simple-dialog-title">
                 Are you sure you want to delete this comment?
             </DialogTitle>
-            <button onClick={(e) => _handleDelete(e)}> yes </button>
-            <button onClick={handleClickNo}> no </button>
+            <DialogContent>
+                <DialogContentText id="alert-dialog-slide-description">
+                    Please proceed with caution. By agreeing, your comment will
+                    be deleted and will be lost forever.
+                </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+                <ThemeProvider theme={theme}>
+                    <Button onClick={handleClickNo} color="secondary">
+                        {" "}
+                        Disagree{" "}
+                    </Button>
+                    <Button onClick={(e) => _handleDelete(e)} color="primary">
+                        {" "}
+                        Agree{" "}
+                    </Button>
+                </ThemeProvider>
+            </DialogActions>
         </Dialog>
     );
 }
