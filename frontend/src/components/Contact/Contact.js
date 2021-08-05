788a84e567b2e0bd9c8edbb606919145e45eefd5
Ann <annie.wang50@yahoo.com>
2021-08-05 17:36:37 -0400

    contact form skeleton

--- frontend/src/components/Contact/Contact.js
+++ frontend/src/components/Contact/Contact.js
@@ -1,10 +1,46 @@
 import navbar from "../../assets/navbar.png"
+import envelope from "../../assets/envelope.png"
+
 import "./Contact.css"
 
 export default function Contact() {
   return (
     <div className="Contact" style={{backgroundImage: `url(${navbar})`}}>
-      <div>Contact Us page</div>
+      <div className="contact-img">
+        <img src={envelope} alt="Envelope"></img>
+      </div>
+      <form>
+        <div className="section-title">Have Questions?</div>
+        <div className="input-name">
+          <input
+            type="text"
+            name="first_name"
+            placeholder="first name"
+          />
+          <input
+            type="text"
+            name="last_name"
+            placeholder="last name"
+          />
+        </div>
+        <div>
+          <input
+            type="email"
+            name="email"
+            placeholder="email"
+          />
+        </div>
+
+        <div>
+          <input 
+            type="text" 
+            name="message"
+            placeholder="message"
+          >
+          </input>
+        </div>
+        <button className="contact-btn">send</button>
+      </form>
     </div>
   )
 }
