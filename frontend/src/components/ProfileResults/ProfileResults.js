cda8a4e792d92c52a4c72dde11cde8c95d2894b8
Ann <annie.wang50@yahoo.com>
2021-08-06 18:48:38 -0400

    order profile search results

--- frontend/src/components/ProfileResults/ProfileResults.js
+++ frontend/src/components/ProfileResults/ProfileResults.js
@@ -1,36 +1,38 @@
 import { Link } from "react-router-dom"
 
+import navbar from "../../assets/navbar.png"
 import tempImg from "../../assets/tempProfileImg.png";
 import "./ProfileResults.css"
 
 export default function ProfileResults({ user, profile, allProfiles, profileTerm, setProfileId }) {
-  console.log(profileTerm)
-  console.log(allProfiles)
   return (
-    <div className="ProfileResults">
+    <div className="ProfileResults" style={{backgroundImage: `url(${navbar})`}}>
       <div className="section-title">Profile Search Results: {profileTerm}</div>
-      {allProfiles.length > 0 ?
-        allProfiles[0]
-          .filter((r) => (
-            r.first_name.toLowerCase().includes(profileTerm.toLowerCase()) ||
-            r.last_name.toLowerCase().includes(profileTerm.toLowerCase()) ||
-            r.username.toLowerCase().includes(profileTerm.toLowerCase())
-          ))
-          
-          .map((r) => (
-            <Link key={r.id} to={`/profile/${r.id}`} onClick={e => setProfileId(r.id)}>
-              {allProfiles[1]
-                .filter((p) => p.id === r.id)
-                .map((p) => (
-                  <img src={p.image_url ? p.image_url : tempImg} alt={`${r.first_name} profile img`}></img>
-                ))
-              } 
-              <div>{r.first_name} {r.last_name}</div>
-            </Link>
-          ))
+      <div>
+        {allProfiles.length > 0 ?
+          allProfiles[0]
+            .filter((r) => (
+              r.first_name.toLowerCase().includes(profileTerm.toLowerCase()) ||
+              r.last_name.toLowerCase().includes(profileTerm.toLowerCase()) ||
+              r.username.toLowerCase().includes(profileTerm.toLowerCase())
+            ))
+            
+            .map((r, i) => (
+              <Link key={r.id} to={`/profile/${r.id}`} onClick={e => setProfileId(r.id)} className="user">
+                <span>{i + 1}. </span>
+                {allProfiles[1]
+                  .filter((p) => p.id === r.id)
+                  .map((p) => (
+                    <img src={p.image_url ? p.image_url : tempImg} alt={`${r.first_name} profile img`}></img>
+                  ))
+                } 
+                <span>{r.first_name} {r.last_name}</span>
+              </Link>
+            ))
 
-        : null
-      }
+          : null
+        }
+      </div>
     </div>
   )
 }
